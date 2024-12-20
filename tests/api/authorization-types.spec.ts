040d75753280cf213d9e0e7a67c78d9e66640d15
Ozlem Korpe <25618760+ozlemkorpe@users.noreply.github.com>
2024-12-21 02:06:21 +0300

    Removed unnecessery lines

--- tests/api/authorization-types.spec.ts
+++ tests/api/authorization-types.spec.ts
@@ -1,13 +1,5 @@
 import { test, expect } from '@playwright/test';
 
-// Correct Tokens
-const basic = 'YWRtaW46MTIzNDU2'; 
-const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImtpZCI6InNlY3JldC50eHQifQ.eyJ1c2VyIjoidGVzdCJ9.jqBFzyBB68KWiOvEJhcaDgMY0Gea-t0KNnf-fR2Ioyc';
-
-//Incorrect Tokens
-const incorrectBasic = 'token1'; 
-const incorrectToken = 'token2';
-
 test.describe('Authorization checks', () => {
 
   test('Try to authenticate with incorrect Auth Type', async ({ request }) => {

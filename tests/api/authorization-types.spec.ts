abd6094ff56f45b18de906a628571f4900a3fccb
Ozlem Korpe <25618760+ozlemkorpe@users.noreply.github.com>
2024-12-22 19:17:07 +0300

    Fix broken authorization case

--- tests/api/authorization-types.spec.ts
+++ tests/api/authorization-types.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Authorization checks', () => {
-
+  const basic = 'ozlem'; 
   test('Try to authenticate with incorrect Auth Type', async ({ request }) => {
     const response = await request.get('http://rest.testsparker.com/{auth_type}/api/comments', { 
       headers: {
